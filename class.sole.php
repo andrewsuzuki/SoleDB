25270110878e33154a209b5efc1b5a6e606d5282
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2013-10-17 00:26:33 -0400

    Created class file

---
+++ class.sole.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * SoleDB Single Class
+ *
+ * Handles all flat database reading and writing.
+ *
+ * @package SoleDB
+ * @version 0.5
+ * @author  Andrew Suzuki <andrew.b.suzuki@gmail.com>
+ * @link    https://github.com/andrewsuzuki/SoleDB
+ */
+
+class Sole {
+
+    
+
+}
+
+?>
